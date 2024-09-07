757482bf4b68e895086de22c893374f151a725f7
Pratham Kumar <prk61dlw@gmail.com>
2024-09-07 23:47:54 +0530

    added menu driven elements in main class

--- src/Admin_TablesManager.java
+++ src/Admin_TablesManager.java
@@ -1,14 +1,37 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 public class Admin_TablesManager {
+    String url = "jdbc:postgresql://localhost:5432/";
+    String db = "librarymanagement";
+    String username = "postgres";
+    String password = "ADMIN123";
+
     public static void main(String[] pk) {
         Admin_TablesManager tablesManger = new Admin_TablesManager();
-        String tableName = "Student";
-        tablesManger.createTable(tableName);
-        System.out.println("");
+
+//        tablesManger.createTableBooks();
+        tablesManger.createTableUsers();
+    }
+
+    private void createTableUsers() {
 
     }
 
-    private void createTable(String tableName) {
-
+    private void createTableBooks() {
+        try {
+            Connection conn = DriverManager.getConnection(url+db,username,password);
+            String query = "Create table Books (BookID int primary key, BookName varchar (30) not null, YOP date not null)";
+            Statement stm = conn.createStatement();
+            boolean check = stm.execute(query);
+            if (check)
+                System.out.println("Done");
+            else
+                System.out.println("Gotcha");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- src/LibraryManagementSystemMain.java
+++ src/LibraryManagementSystemMain.java
@@ -1,3 +1,38 @@
-public class LibraryManagementSystemMain {
+import java.util.Scanner;
 
+public class LibraryManagementSystemMain {
+    public static void main(String[] pk) {
+        LibraryMangement lm = new LibraryMangement();
+        Scanner sc = new Scanner(System.in);
+        boolean runner = true;
+        while (runner) {
+            System.out.println("1. Add Book");
+            System.out.println("2. Update Book");
+            System.out.println("3. Check Availability Book");
+            System.out.println("4. Show available books");
+            System.out.println("Enter your choice: ");
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+//                    add method
+                    break;
+                case 2:
+//                     add method
+                    break;
+                case 3:
+//                    add method
+                    break;
+                case 4:
+//                    add method
+                case 5:
+                    System.out.println("Bye and keep learning");
+                    runner = false;
+                    break;
+
+                default:
+                    System.out.println("Please choose correct option");
+            }
+
+        }
+    }
 }
